6961a91b875137560d5dbc3fb7b9351dd7791cb0
jiehong <jiehong0914@gmail.com>
2020-09-15 14:57:43 +0800

    try try

--- clock.py
+++ clock.py
@@ -3,21 +3,33 @@ import requests
 import datetime
 
 
-# 宣告一個排程
+# # 宣告一個排程
 sched = BlockingScheduler()
 
-# 定義排程 : 在周一至周五，每 1 分鐘就做一次 def scheduled_jog()
+# # 定義排程 : 在周一至周五，每 1 分鐘就做一次 def scheduled_jog()
 
-print(datetime.datetime.now().ctime())
+# print(datetime.datetime.now().ctime())
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/1')
+# # @sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/1')
+# # def timed_job_awake_your_app():
+# #     print('awake app every minute.')
+# #     url = 'https://app.herokuapp.com/'
+# #     r = requests.get(url)
+# # print("--> r.content")
+# # print(r.content)
+
+
+# sched.start()  # 啟動排程
+
+
+@sched.scheduled_job('interval', minutes=1)
 def timed_job_awake_your_app():
-    print('awake app every minute.')
-    url = 'https://app.herokuapp.com/'
+    print('awake app every minutes.')
+    url = 'https://line-course-notify.herokuapp.com/'
     r = requests.get(url)
-    # print("--> r.content")
-    # print(r.content)
+    print("--> r.content")
+    print(r.content)
 
 
-sched.start()  # 啟動排程
+sched.start()
