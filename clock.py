69bbd35d8e213da2cdc550de3b1e7418defe0cf2
JieHong-Liu <jiehong0914@gmail.com>
2020-09-16 00:46:22 +0800

    哭阿 我她媽看一整天的log了

--- clock.py
+++ clock.py
@@ -6,13 +6,6 @@ import os
 # # 宣告一個排程
 sched = BlockingScheduler()
 
-if trigger == "interval":
-    params = dict(func=aps_func, trigger="interval",
-                  seconds=seconds, **kwargs)
-elif trigger == "date":
-    params = dict(func=aps_func, trigger="date",
-                  run_date=run_date, **kwargs)
-
 # # 定義排程 : 在周一至周五，每分鐘就做一次 def scheduled_jog()
 
 # print(datetime.datetime.now().ctime())
@@ -23,6 +16,8 @@ def scheduled_job():
     print('awake app every minutes.')
     os.system("gunicorn app:app")
 # 感謝大大救我一命:https://ivanjo39191.pixnet.net/blog/post/179453201-python-django-%E5%AF%A6%E4%BD%9C%28%E4%B8%89%29-%E7%94%A8-abschedule-%E5%9C%A8-heroku-%E5%BE%8C%E5%8F%B0
+    params = dict(func=aps_func, trigger="interval",
+                  seconds=seconds, **kwargs)
 
 
 sched.add_job(**params, max_instances=100)
