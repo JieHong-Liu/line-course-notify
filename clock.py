82c04bf8869cbbcabb4d1501b472009b8d6b1379
JieHong-Liu <jiehong0914@gmail.com>
2020-09-16 00:51:45 +0800

    QQ

--- clock.py
+++ clock.py
@@ -16,9 +16,6 @@ def scheduled_job():
     print('awake app every minutes.')
     os.system("gunicorn app:app")
 # 感謝大大救我一命:https://ivanjo39191.pixnet.net/blog/post/179453201-python-django-%E5%AF%A6%E4%BD%9C%28%E4%B8%89%29-%E7%94%A8-abschedule-%E5%9C%A8-heroku-%E5%BE%8C%E5%8F%B0
-    params = dict(func=aps_func, trigger="interval",
-                  seconds=seconds, **kwargs)
 
 
-sched.add_job(**params, max_instances=100)
 sched.start()
