bb382e8803ab6eb45274d38f651aa8751fa8af92
jiehong <jiehong0914@gmail.com>
2020-09-15 13:15:53 +0800

    try

--- clock.py
+++ clock.py
@@ -1,6 +1,6 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
 import datetime
-from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 # 宣告一個排程
@@ -16,8 +16,14 @@ def timed_job_awake_your_app():
     print('awake app every 10 minutes.')
     url = 'https://line-course-notify.herokuapp.com/'
     r = requests.get(url)
-    print("--> r.content")
-    print(r.content)
+    # print("--> r.content")
+    # print(r.content)
+#  def scheduled_job():
+#     url = "https://line-course-notify.herokuapp.com/"
+#     conn = urllib.request.urlopen(url)
+
+#     for key, value in conn.getheaders():
+#         print(key, value)
 
 
 sched.start()  # 啟動排程
